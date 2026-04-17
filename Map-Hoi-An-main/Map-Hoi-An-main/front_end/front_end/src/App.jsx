36f6a1e3f8dda46a4a4af3fcb8ee31ba383612e7
Hieunnp <nguyenhieu56193@gmail.com>
2026-04-17 23:43:35 +0700

    Add TODO comments for future improvements in frontend

--- Map-Hoi-An-main/Map-Hoi-An-main/front_end/front_end/src/App.jsx
+++ Map-Hoi-An-main/Map-Hoi-An-main/front_end/front_end/src/App.jsx
@@ -14,6 +14,10 @@ const redIcon = new L.Icon({
   iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41]
 });
 
+// TODO: Add loading states for map and audio
+// TODO: Implement offline support for cached data
+// TODO: Add accessibility features for screen readers
+
 const blueIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
